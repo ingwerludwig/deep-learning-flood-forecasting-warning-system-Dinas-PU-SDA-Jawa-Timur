b7e15063271290594efa3e3e5b7dfb6b2e7b2222
ingwerludwig <ingwerflash@gmail.com>
2023-11-05 00:21:02 +0700

    feat: add endpoint for predict except TCN

--- TimeSeriesModel.py
+++ TimeSeriesModel.py
@@ -2,20 +2,20 @@ import pickle
 import os
 import pandas as pd
 from tensorflow.keras.models import load_model
-from preprocess_data import split_sequences_sliding
+
+def load_scaler(scaler_path):
+    with open(scaler_path, "rb") as file:
+        return pickle.load(file)
+
 
 class TimeSeriesModel:
     def __init__(self, model_path, x_scaler_path, y_scaler_path):
         self.model = load_model(model_path)
-        self.x_scaler = self.load_scaler(x_scaler_path)
-        self.y_scaler = self.load_scaler(y_scaler_path)
-    
-    def load_scaler(self, scaler_path):
-        with open(scaler_path, "rb") as file:
-            return pickle.load(file)
-    
+        self.x_scaler = load_scaler(x_scaler_path)
+        self.y_scaler = load_scaler(y_scaler_path)
+
     def predict(self, scaled_data, y_scaler):
-        predictions = y_scaler.inverse_transform(self.model.predict_generator(scaled_data).reshape(-1,1))
+        predictions = y_scaler.inverse_transform(self.model.predict_generator(scaled_data).reshape(-1, 1))
         predictions = pd.DataFrame(predictions)
         predictions = predictions
         predictions = pd.DataFrame(predictions)
@@ -30,14 +30,17 @@ class DhompoDataPreprocessor:
             os.path.join(scaler_dir, f"{model_name}_x_scaler.pkl"),
             os.path.join(scaler_dir, f"{model_name}_y_scaler.pkl")
         )
-    
+
     def preprocess_data(self, df_test, n_steps_in):
-        x_test, _ = split_sequences_sliding(df_test.to_numpy(), n_steps_in, 1)
-        x_test = x_test.reshape(-1, 4)
-        x_test_scaled = self.model.x_scaler.transform(x_test).reshape(-1, n_steps_in, 4)
+        df = df_test
+        df['tanggal'] = pd.to_datetime(df['tanggal'])
+        df.sort_values(by='tanggal', ascending=False)
+        df = df.drop(columns=['tanggal'])
+
+        x_test_scaled = self.model.x_scaler.transform(df.to_numpy()).reshape(-1, n_steps_in, 4)
         return x_test_scaled
-    
-    
+
+
 class PurwodadiDataPreprocessor:
     def __init__(self, model_name, model_dir, scaler_dir):
         self.model = TimeSeriesModel(
@@ -45,30 +48,35 @@ class PurwodadiDataPreprocessor:
             os.path.join(scaler_dir, f"{model_name}_x_scaler.pkl"),
             os.path.join(scaler_dir, f"{model_name}_y_scaler.pkl")
         )
-    
+
     def preprocess_data(self, df_test, n_steps_in):
-        x_test, _ = split_sequences_sliding(df_test.to_numpy(), n_steps_in, 1)
-        x_test = x_test.reshape(-1, 3)
-        x_test_scaled = self.model.x_scaler.transform(x_test).reshape(-1, n_steps_in, 3)
+        df = df_test
+        df['tanggal'] = pd.to_datetime(df['tanggal'])
+        df.sort_values(by='tanggal', ascending=False)
+        df = df.drop(columns=['tanggal'])
+
+        x_test_scaled = self.model.x_scaler.transform(df.to_numpy()).reshape(-1, n_steps_in, 3)
         return x_test_scaled
-               
-model_path = os.path.join(os.getcwd(),"Model")
-scaler_path = os.path.join(os.getcwd(),"Scaler")
+
+
+model_path = os.path.join(os.getcwd(), "Model")
+scaler_path = os.path.join(os.getcwd(), "Scaler")
 
 dhompo_gru_preprocessor = DhompoDataPreprocessor("dhompo_gru", model_path, scaler_path)
 dhompo_lstm_preprocessor = DhompoDataPreprocessor("dhompo_lstm", model_path, scaler_path)
-dhompo_tcn_preprocessor = DhompoDataPreprocessor("dhompo_tcn",model_path, scaler_path)
-purwodadi_gru_preprocessor = PurwodadiDataPreprocessor("purwodadi_gru",model_path,scaler_path)
-purwodadi_lstm_preprocessor = PurwodadiDataPreprocessor("purwodadi_lstm",model_path,scaler_path)
-purwodadi_tcn_preprocessor = PurwodadiDataPreprocessor("purwodadi_tcn",model_path,scaler_path)
+# dhompo_tcn_preprocessor = DhompoDataPreprocessor("dhompo_tcn", model_path, scaler_path)
+purwodadi_gru_preprocessor = PurwodadiDataPreprocessor("purwodadi_gru", model_path, scaler_path)
+purwodadi_lstm_preprocessor = PurwodadiDataPreprocessor("purwodadi_lstm", model_path, scaler_path)
+# purwodadi_tcn_preprocessor = PurwodadiDataPreprocessor("purwodadi_tcn", model_path, scaler_path)
+
 
 def get_model(model_name):
     models = {
         "dhompo_gru": dhompo_gru_preprocessor,
         "dhompo_lstm": dhompo_lstm_preprocessor,
-        "dhompo_tcn": dhompo_tcn_preprocessor,
+        # "dhompo_tcn": dhompo_tcn_preprocessor,
         "purwodadi_gru": purwodadi_gru_preprocessor,
         "purwodadi_lstm": purwodadi_lstm_preprocessor,
-        "purwodadi_tcn": purwodadi_tcn_preprocessor
+        # "purwodadi_tcn": purwodadi_tcn_preprocessor
     }
     return models.get(model_name)
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from TimeSeriesModel import get_model
-from preprocess_data import get_data
+import pandas as pd
+import json
 
 app = Flask(__name__)
 
@@ -8,31 +9,81 @@ app = Flask(__name__)
 def home():
     return "<h1>Hello</h1>"
 
-# /api/predict?daerah=dhompo&model=lstm
-@app.route("/api/predict")
+# Endpoint
+# /api/predict
+
+# Request Body
+# {
+#     "daerah" : "dhompo",
+#     "model" : "lstm",
+#     "data": [
+#         {
+#             "RC": 0.00,
+#             "RL": 0.00,
+#             "LP": 0.11,
+#             "LD": 0.32,
+#             "tanggal": "2022-09-20 20:00:00"
+#         },
+#         {
+#             "RC": 0.00,
+#             "RL": 0.00,
+#             "LP": 0.11,
+#             "LD": 0.31,
+#             "tanggal": "2022-09-20 21:00:00"
+#         },
+#         {
+#             "RC": 0.00,
+#             "RL": 0.00,
+#             "LP": 0.11,
+#             "LD": 0.30,
+#             "tanggal": "2022-09-20 22:00:00"
+#         },
+#         {
+#             "RC": 0.00,
+#             "RL": 0.00,
+#             "LP": 0.11,
+#             "LD": 0.29,
+#             "tanggal": "2022-09-20 23:00:00"
+#         },
+#         {
+#             "RC": 0.00,
+#             "RL": 0.00,
+#             "LP": 0.11,
+#             "LD": 0.29,
+#             "tanggal": "2022-09-21 00:00:00"
+#         }
+#     ]
+# }
+@app.route("/api/predict", methods=['POST'])
 def api_pred():
-    req_daerah = request.args.get('daerah', type = str)
-    req_model = request.args.get('model', default = "lstm", type = str)
+    input_data = request.get_json()
+    if not input_data:
+        return jsonify({"error": "Input data not provided"}), 400
+
+    data_sensor = input_data.get('data', [])
+    req_daerah = input_data.get('daerah')
+    req_model = input_data.get('model', "lstm")
+
     daerah_model = f"{req_daerah}_{req_model}"
     select_model = get_model(daerah_model)
-    input_data = get_data()
-    
-    if(daerah_model == "dhompo_gru"):
+
+    if daerah_model == "dhompo_gru":
         n_steps_in = 3
-    elif(daerah_model == "dhompo_lstm"):
+    elif daerah_model == "dhompo_lstm":
         n_steps_in = 5
-    elif(daerah_model == "dhompo_tcn"):
+    elif daerah_model == "dhompo_tcn":
         n_steps_in = 5
-    elif(daerah_model == "purwodadi_gru"):
+    elif daerah_model == "purwodadi_gru":
         n_steps_in = 1
-    elif(daerah_model == "purwdoadi_lstm"):
+    elif daerah_model == "purwdoadi_lstm":
         n_steps_in = 2
-    elif(daerah_model == "purwodadi_tcn"):
+    elif daerah_model == "purwodadi_tcn":
         n_steps_in = 2
-    
-    preprocessed_data = select_model.preprocess_data(input_data, n_steps_in)
-    prediction = select_model.predict(preprocessed_data, select_model.model.y_scaler)
-    return prediction
+
+    preprocessed_data = select_model.preprocess_data(pd.DataFrame(data_sensor), n_steps_in)
+    prediction = select_model.model.predict(preprocessed_data, select_model.model.y_scaler)
+    return prediction.to_json()
+
 
 if __name__ == '__main__':
     app.run(host="localhost", port=8000)
